0d09a9c77a0b66e9146b0d47eff24a4a078aa776
SaiKiranReddy17 <89417688+SaiKiranReddy17@users.noreply.github.com>
2021-11-11 21:28:50 -0600

    updated

--- controllers/umbrella.js
+++ controllers/umbrella.js
@@ -15,8 +15,15 @@ exports.umbrella_list = async function(req, res) {
 
  
 // for a specific umbrella. 
-exports.umbrella_detail = function(req, res) { 
-    res.send('NOT IMPLEMENTED: umbrella detail: ' + req.params.id); 
+exports.umbrella_detail = async function(req, res) { 
+    console.log("detail"  + req.params.id) 
+    try { 
+        result = await umbrella.findById( req.params.id) 
+        res.send(result) 
+    } catch (error) { 
+        res.status(500) 
+        res.send(`{"error": document for id ${req.params.id} not found`); 
+    } 
 }; 
  
 // Handle umbrella create on POST. 
@@ -59,6 +66,22 @@ exports.umbrella_delete = function(req, res) {
 }; 
  
 // Handle umbrella update form on PUT. 
-exports.umbrella_update_put = function(req, res) { 
-    res.send('NOT IMPLEMENTED: umbrella update PUT' + req.params.id); 
-};
+exports.umbrella_update_put = async function(req, res) { 
+    console.log(`update on id ${req.params.id} with body 
+${JSON.stringify(req.body)}`) 
+    try { 
+        let toUpdate = await umbrella.findById( req.params.id) 
+        // Do updates of properties 
+        if(req.body.brand)  
+               toUpdate.brand = req.body.brand; 
+        if(req.body.color) toUpdate.color = req.body.color; 
+        if(req.body.cost) toUpdate.cost = req.body.cost; 
+        let result = await toUpdate.save(); 
+        console.log("Sucess " + result) 
+        res.send(result) 
+    } catch (err) { 
+        res.status(500) 
+        res.send(`{"error": ${err}: Update for id ${req.params.id} 
+failed`); 
+    } 
+}; 
